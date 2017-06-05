85922bc1e27313f3273db9e2136148b8d90807ee
Demitroi <checo1085@hotmail.com>
2017-06-05 12:50:34 -0700

    Abrir y Registrar ya funcionan

--- backbone.marionette/obtenerDatos.js
+++ backbone.marionette/obtenerDatos.js
@@ -8,33 +8,64 @@ $(document).ready(function(){
 
   $("#formLoginAgente").on("submit",function(e){
     e.preventDefault();
-    /*console.log("Email:\n",$("#txtEmail").val());
-    console.log("Pass:\n",$("#txtPass").val());*/
-
+    console.log("Email:\n",$("#txtEmail").val());
+    console.log("Pass:\n",$("#txtPass").val());
   });
 
   $("#FormOpen").on("submit",function(e){
     e.preventDefault();
-    console.log("Tema:\n",$("#txtTema").val());
-    console.log("Producto:\n",$("#txtProducto").val());
-    console.log("Correo:\n",$("#txtEmail").val());
-    console.log("Nombre:\n",$("#txtNombre").val());
-    console.log("Telefono:\n",$("#txtTelefono").val());
-    console.log("EXT:\n",$("#txtExt").val());
-    console.log("Titulo:\n",$("#txtTitulo").val());
-    console.log("Detalle:\n",$("#txtDetalle").val());
+    if($("#txtTema")[0].selectedIndex == 0 || $("#txtProducto")[0].selectedIndex == 0 || $("#txtTitulo").val() == "" || $("#txtDetalle").val() == ""){
+        console.log("Asegurate de seleccionar tema, producto y no dejar cuadros sin llenar");
+        return;
+    }
+    var noCont = 0;
+    $.ajax({
+        async: false,
+        url: "http://localhost:5555/api/contacts?search="+$("#txtEmail").val(),
+        type: "GET",
+        success: function (data) {
+            if(data.rows.length == 1) {
+                noCont = data.rows[0].idcont;
+            }
+        },
+        error: function () {
+            console.log("no hay conexion al server");
+        }
+    });
+    if(noCont > 0){
+        $.ajax({
+            url: "http://localhost:5555/api/orders",
+            type: "POST",
+            dataType: "json",
+            data: JSON.stringify({
+                "idcont": noCont,
+                "obs": $("#txtTitulo").val(),
+                "duracion": 15,
+                "mensaje": $("#txtDetalle").val(),
+                "idclas": $("#txtTema").val(),
+                "idprod": $("#txtProducto").val()
+            }),
+            success: function (data) {
+                console.log("Se registró el ticket con el ID: "+data.data);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }
+    else{
+        console.log("El correo no esta registrado");
+    }
   });
 
   $("#formView").on("submit",function(e){
     e.preventDefault();
-    //console.log("Email:\n",$("#txtEmail").val())
-    //console.log("Ticket:\n",$("#txtTicket").val());
+    var ticket;
       $.ajax({
           url : "http://localhost:5555/api/orders/"+$("#txtTicket").val(),
           type: "GET",
           success: function(data, textStatus, jqXHR)
           {
-              var ticket;
               if($("#txtEmail").val().toLowerCase() == data.email.toLowerCase())
               {
                   ticket = new App.Views.vistaTicketCorrecto();
@@ -52,20 +83,62 @@ $(document).ready(function(){
           {
               ticket = new App.Views.vistaTicketIncorrecto();
               ticket.render();
-              console.log("No. Ticket no encontrado");
+              console.log("NO ENCONTRADO");
           }
       });
   });
 
   $("#formCreate").on("submit",function(e){
     e.preventDefault();
-    console.log("Email:\n",$("#txtEmail").val());
-    console.log("Nombre:\n",$("#txtNombre").val());
-    console.log("Télefono:\n",$("#txtTelefono").val());
-    console.log("EXT:\n",$("#txtExt").val());
-    console.log("Zona Horaria:\n",$("#txtZona").val());
-    console.log("Horario de Verano\n",$("#txtVerano").is(":checked"));
-    console.log("Contraseña 1:\n",$("#txtPass1").val());
-    console.log("Contraseña 2:\n",$("#txtPass2").val());
+    if($("#txtNombre").val() == "" || $("#txtEmpresa") == "" || $("#txtEmail").val()== "" || $("#txtTelefono").val() == "" || $("#txtPass1").val() == "" || $("#txtPass2").val() == ""){
+        console.log("Asegurate de llenar los campos");
+        return;
+    }
+    var caEmail;
+    $.ajax({
+        async: false,
+        url: "http://localhost:5555/api/contacts?search="+$("#txtEmail").val(),
+        type: "GET",
+        success: function (data) {
+            caEmail = data.rows.length;
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+    switch (caEmail){
+        case 0:
+            console.log("Correo sin registrar");
+            break;
+        case 1:
+            console.log("Correo registrado");
+            return; break;
+        default:
+            console.log("Asegurate de escribir tu correo");
+            return; break;
+    }
+    if($("#txtPass1").val() != $("#txtPass2").val()){
+        console.log("Las contraseñas no coinciden");
+        return;
+    }
+    $.ajax({
+        url: "http://localhost:5555/api/contacts",
+        type: "POST",
+        dataType: "json",
+        data: JSON.stringify({
+            "nombre": $("#txtNombre").val(),
+            "empresa": $("#txtEmpresa").val(),
+            "email": $("#txtEmail").val(),
+            "telefono": $("#txtTelefono").val(),
+            "ext": $("#txtExt").val(),
+            "pswd": $("#txtPass1").val()
+        }),
+        success: function (data) {
+            console.log("Registrado con el ID: "+data.data);
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
   });
 });
